5e0967a7ba02562a6e6977a35f32cdd489fc9770
Maria Zudina <maria.zudina@edu.karelia.fi>
2014-10-19 14:52:28 +0300

    Added mainGUI and script

---
+++ Uusi/Assets/Scripts/MainGUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class MainGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void OnGUI () 
+	{
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label ("Bye cruel world");
+		GUILayout.EndHorizontal ();
+	}
+
+}
